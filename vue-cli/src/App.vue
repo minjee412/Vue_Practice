dce2a33ab7ebbf323a6c1cb17f971e3de590135c
minjee412 <minjee412@gmail.com>
2022-04-04 09:04:34 +0900

    2022.04.04

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,28 +1,43 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- ✔ template에는 반드시 태그가 하나만 있어야 한다 !! -->
+  <div>
+    <!-- {{str}} -->
+
+    <!-- <app-header v-bind:프롭스속성이름="상위컴포넌트의데이터이름"/> -->
+    <app-header 
+    v-bind:propsdata="str"
+    v-on:renew="renewStr"
+    />
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
+  import AppHeader from './components/AppHeader.vue'
+  export default {
+  // cli 부터는 data:{}로만 값을 넣으면 에러
+  // 무조건 function(){ 
+  //    return { 
+  //
+  //    }
+  //  } 으로 return에 새로운 객체를 만들어야 한다. 
+  data: function () {
+    return{
+      str: 'Header'
+    }
+  },
   components: {
-    HelloWorld
+    'app-header': AppHeader
+  },
+  props:['propsdata'],
+  methods:{
+    renewStr : function(){
+      this.str = 'hi';
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ vue-cli/src/a.vue
@@ -0,0 +1,29 @@
+// var appHeader = {
+//     template : '<div>header</div>',
+//     methods:{
+//         addNum: function(){
+
+//         }
+//     }
+// }
+
+
+<template>
+  <!-- 🔸HTML🔸 -->
+  <div>header</div>
+</template>
+
+<script>
+export default {
+    // 🔸Javascript🔸 - 인스턴스 옵션
+   methods:{
+        addNum: function(){
+
+        }
+    }
+}
+</script>
+
+<style>
+    /* 🔸CSS🔸 */
+</style>
